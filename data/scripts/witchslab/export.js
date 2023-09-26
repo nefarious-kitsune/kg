36c53d799fab818968c92c3b0996b140ce0f2afd
kitsune <aow.anti.hero@gmail.com>
2023-09-26 11:54:02 +0800

    exported witch's lab data

---
+++ data/scripts/witchslab/export.js
@@ -0,0 +1,83 @@
+import fs from 'fs';
+const fileData = fs.readFileSync('data.csv', 'utf-8').split('\n');
+const importedData = [];
+
+for (let i = 1; i < fileData.length; i++) {
+  const data = fileData[i].split(',');
+  const bracketIndex = parseInt(data[0]);
+
+  let startLevel = 0;
+  let verified = false;
+
+  if (data[1].length) {
+    startLevel = parseInt(data[1]);
+    verified = true;
+  } else {
+    startLevel = parseInt(data[2]);
+  }
+
+  if (startLevel >= 2000) break;
+ 
+  const cost = parseInt(data[4]);
+
+  importedData.push(
+    {
+      index: bracketIndex,
+      startLevel: startLevel,
+      verified: verified,
+      cost: cost,
+    }
+  )
+}
+
+const exportedJSON = JSON.stringify(importedData, null, '  ');
+const exportedJs = 'const WitchLabUpgradeCost = ' + exportedJSON;
+fs.writeFileSync('../../../docs/mk/witchLabUpgradeCost.json', exportedJSON);
+fs.writeFileSync('../../../docs/mk/witchLabUpgradeCost.js', exportedJs);
+
+const exportedTableRows = [];
+
+for (let i = 1; i < importedData.length; i++) {
+  const startLevel = importedData[i].startLevel;
+  const endLevel =
+    (importedData.length > i+1)?
+    importedData[i+1].startLevel:
+    2000;
+  const cost = importedData[i].cost;
+  const verified = importedData[i].verified;
+
+  exportedTableRows.push(
+`      <tr>
+        <td>${startLevel} → ${startLevel+1}<br>⋮<br>${endLevel-1} → ${endLevel}</td>
+        <td>${cost}</td>
+        <td>Light Reagent</td>
+        <td class="${verified?'verified':'unverified'}">${verified?'Verified':'Extrapolated'}</td>
+       </tr>`
+  )
+}
+
+const htmlTemplate = fs.readFileSync('template.html', 'utf-8');
+const htmlOutput = htmlTemplate.replace(
+  '<!-- Data -->',
+  exportedTableRows.join('\n'),
+);
+
+fs.writeFileSync(
+  '../../../docs/mk/witchLabUpgradeCost.html',
+  htmlOutput,
+  {encoding:'utf8',flag:'w'},
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
