cd88a083c4d5655a8bf1e69d7b07d3125a1b0fed
kitsune <aow.anti.hero@gmail.com>
2023-11-24 16:09:55 +0800

    better ui

--- data/scripts/tools/hero-upgrade-up/build.js
+++ data/scripts/tools/hero-upgrade-up/build.js
@@ -5,13 +5,22 @@ const importFileName = 'template';
 const exportFileName = 'heroUpgradeCalculator';
 
 const mainTemplate = fs.readFileSync(`${importFileName}.html`, 'utf-8');
-const heroRowTemplate = fs.readFileSync('t-hero.html', 'utf-8');
+const heroInputRowTemplate = fs.readFileSync('t-hero.html', 'utf-8');
+const heroOutputRowTemplate = fs.readFileSync('t-hero-calculated.html', 'utf-8');
 
-let heroRows = '';
+let heroInputRows = '';
 for (let idx = 2; idx <= 20; idx++) {
-  heroRows += heroRowTemplate.replaceAll('{{NUMBER}}' , idx);
+  heroInputRows += heroInputRowTemplate.replaceAll('{{NUMBER}}' , idx);
 }
 
-const exportedHtml = mainTemplate.replace('{{HERO ROWS}}', heroRows);
+let heroOutputRows = '';
+for (let idx = 2; idx <= 20; idx++) {
+  heroOutputRows += heroOutputRowTemplate.replaceAll('{{NUMBER}}' , idx);
+}
+
+
+const exportedHtml = mainTemplate
+  .replace('{{HERO INPUT ROWS}}', heroInputRows)
+  .replace('{{HERO OUTPUT ROWS}}', heroOutputRows);
 
 fs.writeFileSync(`${exportDirectory}/${exportFileName}.html`, exportedHtml);
