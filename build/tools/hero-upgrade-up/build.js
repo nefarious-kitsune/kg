982c32f06c54c624c3f48242ae3d3851cf545b20
kitsune <aow.anti.hero@gmail.com>
2023-12-15 10:42:27 +0800

    new templates and optimize output conversion

--- build/tools/hero-upgrade-up/build.js
+++ build/tools/hero-upgrade-up/build.js
@@ -1,23 +1,22 @@
 import fs from 'fs';
 
-const exportDirectory = '../../../../docs/tools';
+const exportDirectory = '../../../docs/tools';
 const importFileName = 'template';
 const exportFileName = 'heroUpgradeCalculator';
 
 const mainTemplate = fs.readFileSync(`${importFileName}.html`, 'utf-8');
-const heroInputRowTemplate = fs.readFileSync('t-hero.html', 'utf-8');
-const heroOutputRowTemplate = fs.readFileSync('t-hero-calculated.html', 'utf-8');
+const heroInputRowTemplate =  fs.readFileSync('t-hero-input.html',  'utf-8');
+const heroOutputRowTemplate = fs.readFileSync('t-hero-output.html', 'utf-8');
 
 let heroInputRows = '';
-for (let idx = 2; idx <= 20; idx++) {
-  heroInputRows += heroInputRowTemplate.replaceAll('{{NUMBER}}' , idx);
+for (let idx = 1; idx <= 20; idx++) {
+  heroInputRows += heroInputRowTemplate.replaceAll('{{INDEX}}' , idx);
 }
 
 let heroOutputRows = '';
-for (let idx = 2; idx <= 20; idx++) {
-  heroOutputRows += heroOutputRowTemplate.replaceAll('{{NUMBER}}' , idx);
+for (let idx = 1; idx <= 20; idx++) {
+  heroOutputRows += heroOutputRowTemplate.replaceAll('{{INDEX}}' , idx);
 }
-
 
 const exportedHtml = mainTemplate
   .replace('{{HERO INPUT ROWS}}', heroInputRows)
