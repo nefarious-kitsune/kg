879d3ae43a7b671df2eb6846f127fe8b9f8421a1
kitsune <aow.anti.hero@gmail.com>
2024-03-13 09:50:46 +0800

    enabling eslint and making eslint happy

--- build/base/copy-static.js
+++ build/base/copy-static.js
@@ -1,7 +1,7 @@
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
+import {fileURLToPath} from 'url';
+import {dirname, resolve} from 'path';
 import fs from 'fs';
-import { getFilesFromDir } from '../utils/file-utils.js'
+import {getFilesFromDir} from '../utils/file-utils.js';
 
 const ModulePath = dirname(fileURLToPath(import.meta.url));
 const ProjectPath = resolve(ModulePath, '../../');
@@ -13,13 +13,18 @@ const ProjectPath = resolve(ModulePath, '../../');
 export function copyStatic(subDir) {
   const srcBasePath = resolve(ProjectPath, './source/', subDir);
   const destBasePath = resolve(ProjectPath, './docs/', subDir);
-  const srcFilePaths = getFilesFromDir(srcBasePath, ['.css', '.html', '.js'], 6);
+  const srcFilePaths = getFilesFromDir(
+      srcBasePath,
+      ['.css', '.html', '.js'],
+      6,
+  );
+
   srcFilePaths.forEach((relPath) => {
     const srcPath = srcBasePath + relPath;
     const destPath = destBasePath + relPath;
     const destDir = dirname(destPath);
-    if (!fs.existsSync(destDir)) fs.mkdirSync(destDir, { recursive: true })
+    if (!fs.existsSync(destDir)) fs.mkdirSync(destDir, {recursive: true});
     fs.copyFileSync(srcPath, destPath, fs.constants.COPYFILE_FICLONE);
     console.log(`${relPath} copied`);
-  })
+  });
 }
