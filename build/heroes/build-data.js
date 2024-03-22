f88f3f32717df4ae31b93ededb6f7ef67ea6b7fb
kitsune <aow.anti.hero@gmail.com>
2024-03-22 16:42:32 +0800

    tweaking

--- build/heroes/build-data.js
+++ build/heroes/build-data.js
@@ -66,6 +66,8 @@ const ExportPath = resolve(ProjectPath, './docs/heroes/');
  * @property {HeroBonus} bonus - Hero total bonus when fully promoted
  * @property {HeroRating} rating - Ranking the hero at specific roles
  * @property {HeroAcquisition} acquisition - How to acquire
+ * @property {number} [ranking] - a ranking score based on ratings
+ * @property {number} tier - tier based ranking score
  */
 
 /** @type {HeroData[]} */
@@ -390,7 +392,10 @@ export function buildDatabase() {
       element, rarity, name,
       skill1, skill2, skill3, skill4, skill5,
       freePick,
-    ] = row.split('\t');
+    ] = row.split('\t').map((s) => s.trim());
+
+    // Skip blank line from copy/paste
+    if (element === '') return;
 
     /** @type {HeroData} */
     const heroData = {
@@ -408,12 +413,6 @@ export function buildDatabase() {
         'gathering': 0,
         'load': 0,
         'offline': 0,
-      },
-      rating: {
-        attacking: 0,
-        defending: 0,
-        hunting: 0,
-        mining: 0,
       },
       acquisition: {
         'normal': 0,
@@ -476,44 +475,82 @@ function calcRating() {
   HeroBase.forEach((heroData) => {
     const bonus = heroData.bonus;
 
+    // Proper scaling
+    const scale =
+      (point, max) => (point === 0)?0:(0.5 + 0.5*(point-10)/(max-10));
+
     // Round to nearest 0.5
     const round = (num) => Math.round(num*2)/2;
 
-    // Rating of a hero's ability in ATTACKING castles and towers, based on
-    //   1) how quickly can the hero reach the target (Rapid March),
-    //   2) can the hero defend home city after returning? (Regeneration),
-    //   3) unit-power boost (Guerrilla Master)
-    //   4) saving on healing scroll (First Aid)
-    const attackRating = round(
-        4 * (bonus.march / maxBonus.march) +
-        3 * (bonus.regeneration / maxBonus.regeneration) +
-        3 * (bonus['unit-power'] / maxBonus['unit-power']) +
-        1 * (bonus.recovery / maxBonus.recovery),
+    /**
+     * Rating of a hero's ability in ATTACKING castles and towers, based on
+     *   1) how quickly can the hero reach the target (Rapid March),
+     *   2) can the hero defend home city after returning? (Regeneration),
+     *   3) unit-power boost (Guerrilla Master)
+     *   4) saving on healing scroll (First Aid)
+     */
+    let attackRating = round(
+        4.0 * scale(bonus.march, maxBonus.march) +
+        3.0 * scale(bonus.regeneration, maxBonus.regeneration) +
+        3.0 * scale(bonus['unit-power'], maxBonus['unit-power']) +
+        2.5 * scale(bonus.recovery, maxBonus.recovery),
     );
 
-    heroData.rating.attacking = Math.min(10, attackRating);
+    attackRating = Math.min(10, attackRating);
 
     // Rating of a hero's ability in DEFENDING towers, based on
     //   1) health recovered after a successful defense? (Regeneration),
     //   2) unit-power boost (Guerrilla Master)
     //   3) saving on healing scroll (First Aid)
-    heroData.rating.defending = round(
-        4 * (bonus.regeneration / maxBonus.regeneration) +
-        4 * (bonus['unit-power'] / maxBonus['unit-power']) +
-        2 * (bonus.recovery / maxBonus.recovery),
+    const defenseRating = round(
+        4 * scale(bonus.regeneration, maxBonus.regeneration) +
+        4 * scale(bonus['unit-power'], maxBonus['unit-power']) +
+        2 * scale(bonus.recovery, maxBonus.recovery),
     );
 
-    heroData.rating.hunting = round(
-        2 * (bonus.march / maxBonus.march) +
-        3 * (bonus.AP / maxBonus.AP),
+    const huntingRating = round(
+        1 * scale(bonus.march, maxBonus.march) +
+        4 * scale(bonus.AP, maxBonus.AP),
     );
 
-    heroData.rating.mining = round(
-        1.0 * (bonus.march / maxBonus.march) +
-        2.5 * (bonus.gathering / maxBonus.gathering) +
-        1.5 * (bonus.load / maxBonus.load),
+    const miningRating = round(
+        1.0 * scale(bonus.march, maxBonus.march) +
+        2.5 * scale(bonus.gathering, maxBonus.gathering) +
+        1.5 * scale(bonus.load, maxBonus.load),
     );
+
+    heroData.rating = {
+      attacking: attackRating,
+      defending: defenseRating,
+      hunting: huntingRating,
+      mining: miningRating,
+    };
+
+    // Ranking score for determining sorting order and hero tier
+    let rankingScore = attackRating;
+
+    // Bump up the ranking score if the hero has utility values
+    if (huntingRating >= 4) rankingScore += 1;
+    else if (huntingRating >= 2) rankingScore += 0.5;
+    else if (miningRating >= 4) rankingScore += 1.0;
+    else if (miningRating >= 2) rankingScore += 0.5;
+
+    if (rankingScore >= 9.5) heroData.tier = 'S';
+    else if (rankingScore >= 8.5) heroData.tier = 'A';
+    else if (rankingScore >= 7.5) heroData.tier = 'B';
+    else if (rankingScore >= 6) heroData.tier = 'C';
+    else if (rankingScore >= 4) heroData.tier = 'D';
+    else if (rankingScore >= 2) heroData.tier = 'E';
+    else if (rankingScore >= 1) heroData.tier = 'F';
+
+    // Add additional decimal digit to help with sorting
+    // heroData.ranking = rankingScore + (defenseRating / 20);
+    heroData.ranking = rankingScore;
   });
+
+  // sort database and remove temporary sorting index
+  // HeroBase.sort((a, b) => b.ranking - a.ranking);
+  HeroBase.forEach((heroData) => delete heroData.ranking);
 }
 
 /**
@@ -521,11 +558,23 @@ function calcRating() {
  */
 function saveRating() {
   const content =
-      'Element\tRarity\tHero\tFighting\tDefending\t\Hunting\tMining\n' +
+      [
+        'Element',
+        'Rarity',
+        'Tier',
+        // 'Ranking',
+        'Hero',
+        'Attacking',
+        'Defending',
+        'Hunting',
+        'Mining',
+      ].join('\t') + '\n' +
       HeroBase
           .map((heroData) => (
             heroData.element + '\t' +
             heroData.rarity + '\t' +
+            heroData.tier + '\t' +
+            // heroData.ranking + '\t' +
             heroData.name + '\t' +
             heroData.rating.attacking + '\t' +
             heroData.rating.defending + '\t' +
