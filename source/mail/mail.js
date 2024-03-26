a196bfb235c795228ad03aa542474c5dd0c0eaf2
kitsune <aow.anti.hero@gmail.com>
2024-03-26 14:25:21 +0800

    UI improvement

---
+++ source/mail/mail.js
@@ -0,0 +1,60 @@
+// eslint-env browser
+/* eslint-disable no-unused-vars */
+
+let inputElement;
+let previewElement;
+
+document.addEventListener('DOMContentLoaded', (e) => {
+  inputElement = document.getElementById('input');
+  previewElement = document.getElementById('output');
+  validateInput();
+});
+
+
+/**
+ * Change mail mode
+ */
+function switchMode() {
+  if (document.getElementById('dark-mode').checked) {
+    previewElement.classList.remove('mail');
+    previewElement.classList.add('message');
+  } else {
+    previewElement.classList.remove('message');
+    previewElement.classList.add('mail');
+  }
+}
+
+/**
+ * Validate input and generate a preview
+ */
+function validateInput() {
+  switchMode();
+  const inputText = inputElement.value;
+  try {
+    const previewText = parser.parse(inputText);
+    previewElement.classList.remove('error');
+    previewElement.innerHTML = previewText;
+  } catch (e) {
+    previewElement.innerHTML = 'Your message contains an error!';
+    previewElement.classList.add('error');
+  }
+}
+
+/**
+ * Copy input text to the clipboard
+ */
+function copyInput() {
+  // from https://stackoverflow.com/questions/1173194/
+  if (document.selection) { // IE
+    const range = document.body.createTextRange();
+    range.moveToElementText(inputElement);
+    range.select();
+  } else if (window.getSelection) {
+    const range = document.createRange();
+    range.selectNode(inputElement);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+  }
+
+  navigator.clipboard.writeText(inputElement.innerText);
+}
