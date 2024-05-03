4d1894a567267b7860dff5982aa9b8d69f3177c4
kitsune <aow.anti.hero@gmail.com>
2024-05-03 11:57:25 +0800

    auto build vip info

--- build/p2p/build-vip-tables.js
+++ build/p2p/build-vip-tables.js
@@ -2,58 +2,108 @@ import {fileURLToPath} from 'url';
 import {dirname, resolve} from 'path';
 import {writeFileSync, readFileSync} from 'fs';
 
+import {processHtml} from '../base/process-html.js';
+
 const ModulePath = dirname(fileURLToPath(import.meta.url));
 const ProjectPath = resolve(ModulePath, '../../');
 
-const tsvFilePath = resolve(ProjectPath, 'source/p2p/vip/__vip-perks.tsv');
+const srcBasePath = resolve(ProjectPath, './source/');
+const destBasePath = resolve(ProjectPath, './docs/');
+
+const tsvFilePath = resolve(srcBasePath, 'p2p/vip/vip-perks.tsv');
 const tsvData = readFileSync(tsvFilePath, {encoding: 'utf8'})
     .split('\n')
     .map((row) => row.split('\t'));
 
+const templateFilePath = resolve(srcBasePath, 'p2p/vip/__perks.html');
+const template = readFileSync(templateFilePath, {encoding: 'utf8'});
 
 /**
  * something
  * @param {number} level - VIP Level
  */
-function buildTemplate(level) {
-  const rows = [];
-  tsvData.forEach((row) => {
+function buildPage(level) {
+  const prevLevel = (level <= 1)?19:(level-1);
+  const nextLevel = (level >= 19)?1:(level+1);
+
+  const vipPoints = tsvData[1][level + 1];
+  const pt = parseInt(String(vipPoints).replace(',', ''));
+  const iapSpending = (pt / 10)
+      .toLocaleString('en-US', {maximumFractionDigits: 0});
+  const gcSpending = (pt * 10 / 110) // 100 GC = 10 VIP points, 110 GC = $1.00
+      .toLocaleString('en-US', {maximumFractionDigits: 0});
+
+  let content = template
+      .replaceAll('{{LEVEL}}', level)
+      .replaceAll('{{PREVIOUS LEVEL}}', prevLevel)
+      .replaceAll('{{NEXT LEVEL}}', nextLevel)
+      .replaceAll('{{VIP POINTS}}', vipPoints)
+      .replaceAll('{{IAP SPENDING}}', iapSpending)
+      .replaceAll('{{GC SPENDING}}', gcSpending)
+      ;
+
+  let perkTable = '';
+
+  const APLimit = tsvData[2][level + 1];
+  const RecoveryReduction = tsvData[3][level + 1];
+  // Recovery speed: APs / minute
+  const RecoverySpeed = 3.6 * (100 + parseInt(RecoveryReduction)) / 100;
+  const RecoveryTime = Math.floor((APLimit - 15) / RecoverySpeed);
+  const RHours = Math.floor(RecoveryTime / 60);
+  const RMinutes = RecoveryTime - (RHours * 60);
+  content = content
+      .replaceAll('{{AP LIMIT}}', APLimit)
+      .replace('{{AP RECOVERY TIME}}', RHours + 'h ' + RMinutes + 'm')
+  ;
+
+  for (let rowIdx = 4; rowIdx <= 16; rowIdx++) {
+    const row = tsvData[rowIdx];
+
     const desc = row[0];
+    let prev = row[level];
     let value = row[level + 1];
-    if (value === '') {
-      rows.push(
-          '<div class="perk-row na"',
-          `  ><div class="perk-desc">${desc}</div`,
-          `  ><div class="perk-value">&mdash;</div`,
-          '></div>',
-      );
+
+    let unverified = false;
+    let na = false;
+
+    if (prev === '') {
+      prev = '&mdash;';
     } else {
-      let unverified = false;
+      if (prev.indexOf('?') !== -1) value = value.replace('?', '');
+    }
+
+    if (value === '') {
+      value = '&mdash;';
+      na = true;
+    } else {
       if (value.indexOf('?') !== -1) {
         unverified = true;
         value = value.replace('?', '');
       }
-      if (value.indexOf('%') !== -1) {
-        if (value.charAt(0) !== '-') value = '+' + value;
-      }
-      const valueText =
-        (unverified)?
-        `<span class="unverified">${value}<span>`:
-        value;
-      rows.push(
-          '<div class="perk-row"',
-          `  ><div class="perk-desc">${desc}</div`,
-          `  ><div class="perk-value">${valueText}</div`,
-          '></div>',
-      );
     }
-  });
+    const valueText =
+      (unverified)?
+      `<span class="unverified">${value}<span>`:
+      value;
 
-  writeFileSync(
-      resolve(ProjectPath, `source/p2p/vip/__templates/--perks-${level}.tsv`),
-      rows.join('\n'),
-  );
+    const changed = (value !== prev);
+
+    perkTable +=
+        `<div class="perk-row${na?' na':''}${changed?' changed':''}"` + '\n' +
+        `  ><div class="perk-desc">${desc}</div` + '\n' +
+        `  ><div class="prev-value">${prev}</div` + '\n' +
+        `  ><div class="this-value">${valueText}</div` + '\n' +
+        '></div>\n';
+  };
+
+  content = content
+      .replace('{{PERK TABLE}}', perkTable);
+
+  content = processHtml(content, templateFilePath, srcBasePath).source;
+
+  writeFileSync( resolve(destBasePath, `p2p/vip/${level}.html`), content);
 }
 
-buildTemplate(0);
-buildTemplate(1);
+for (let level = 1; level <= 19; level++) {
+  buildPage(level);
+}
