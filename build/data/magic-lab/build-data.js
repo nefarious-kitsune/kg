531293b9fb94d76017c900f596beb5c01eb25874
kitsune <aow.anti.hero@gmail.com>
2024-08-01 15:50:30 +0800

    export data

---
+++ build/data/magic-lab/build-data.js
@@ -0,0 +1,88 @@
+import {fileURLToPath} from 'url';
+import {dirname, resolve} from 'path';
+import {readFileSync, writeFileSync} from 'fs';
+
+const ModulePath = dirname(fileURLToPath(import.meta.url));
+const ProjectPath = resolve(ModulePath, '../../');
+const DataPath = resolve(ProjectPath, '../data/magic-lab/');
+const ExportPath = resolve(ProjectPath, '../docs/magic-lab/');
+
+const database = {
+  'title': 'Witch\'s Lab Upgrade Cost and Power',
+  'power-data': [],
+  'upgrade-data': [],
+};
+
+/**
+ * Import TSV data and compile it to structured data
+ */
+function buildDatabase() {
+  const tsvFilePath = resolve(DataPath, 'light-regent.tsv');
+  const rows = readFileSync(tsvFilePath, {encoding: 'utf8'}).split('\n');
+
+  let totalRegentCost = 0;
+
+  rows.shift(); // Remove header row
+  rows.pop(); // Remove last row
+
+  rows.forEach((row, index) => {
+    const entries = row.split('\t').map((s) => s.trim());
+    const currentLevel = parseInt(entries[0]);
+    const nextLevel = currentLevel + 1;
+
+    let regentCost = 0;
+    let regentCostVerified;
+
+    if (entries[4] === '') {
+      regentCost = parseInt(entries[5]);
+      regentCostVerified = false;
+    } else {
+      regentCost = parseInt(entries[4]);
+      regentCostVerified = true;
+    }
+
+    totalRegentCost += regentCost;
+
+    let power = 0;
+    let powerVerified;
+
+    if (entries[1] === '') {
+      power = 1400 * totalRegentCost;
+      powerVerified = false;
+    } else {
+      power = parseInt(entries[1].replaceAll(',', ''));
+      powerVerified = true;
+    }
+
+    database['power-data'].push({
+      level: currentLevel,
+      power: power,
+      verified: powerVerified,
+    });
+
+    database['upgrade-data'].push({
+      'from': currentLevel,
+      'to': nextLevel,
+      'light-regent': regentCost,
+      'verified': regentCostVerified,
+    });
+  }); // for each
+
+  database['power-data'].push({
+    level: 2000,
+    power: 1400000000,
+    verified: true,
+  });
+}
+
+/**
+ * Save Database
+ */
+function saveDatabase() {
+  const _json = JSON.stringify(database, null, '  ') + '\n';
+
+  writeFileSync(resolve(ExportPath, 'magic-lab.json'), _json);
+}
+
+buildDatabase();
+saveDatabase();
