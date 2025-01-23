2cbc6a99d645a2995cd2054993ecdb3d50c6dbc2
kitsune <aow.anti.hero@gmail.com>
2025-01-23 12:09:40 +0800

    split tables

--- source/city/witchs-lab/__build/build-light-upgrade-table.js
+++ source/city/witchs-lab/__build/build-light-upgrade-table.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import {fileURLToPath} from 'url';
 import {dirname, resolve} from 'path';
 import {readFileSync, writeFileSync} from 'fs';
@@ -5,13 +6,20 @@ import {readFileSync, writeFileSync} from 'fs';
 const ModulePath = dirname(fileURLToPath(import.meta.url));
 const ProjectPath = resolve(ModulePath, '../../../../');
 const TemplatePath = resolve(ModulePath, '../__templates/');
-const ExportPath = resolve(ProjectPath, './docs/city/witchs-lab/');
+const ExportPath = resolve(ProjectPath, './docs/city/witchs-lab/light-magic/');
+const ExportFilePath = resolve(ExportPath, './light-magic-upgrade.tsv');
 
 import {database} from './build-light-data.js';
 
-/** HTML for upgrading one level */ const table1 = [];
-/** TSV for upgrading one level */ const tsv1 = [];
-/** HTML table for upgrading to max */ const table2 = [];
+/** HTML for upgrading one level */ const stepTable1 = [];
+/** HTML for upgrading one level */ const stepTable2 = [];
+/** HTML for upgrading one level */ const stepTable3 = [];
+/** HTML for upgrading one level */ const stepTable4 = [];
+/** HTML table for upgrading to max */ const maxTable1 = [];
+/** HTML table for upgrading to max */ const maxTable2 = [];
+/** HTML table for upgrading to max */ const maxTable3 = [];
+/** HTML table for upgrading to max */ const maxTable4 = [];
+/** TSV for upgrading one level */ const tsv = [];
 
 /**
  * Build upgrade table
@@ -19,7 +27,8 @@ import {database} from './build-light-data.js';
 function buildUpgradeTable() {
   const totalRegentCost = 1400000000 / 1400;
   let cumulativeRegentCost = 0;
-  tsv1.push(['from', 'to', 'light regent cost', 'verified'].join('\t'));
+  // Build header row
+  tsv.push(['from', 'to', 'light regent cost', 'verified'].join('\t'));
 
   const unverifiedMarker = '<img\n' +
       '  src="/assets/emojis/4x/question-mark.png"\n' +
@@ -30,51 +39,47 @@ function buildUpgradeTable() {
       {encoding: 'utf8'},
   );
 
-  database['upgrade-data'].forEach((entry) => {
+  const makeRow = (entry, stepTable, maxTable) => {
     const regentCost = entry['light-regent'];
     const maxRegentCost = totalRegentCost - cumulativeRegentCost;
-    const marker = entry.verified?'':unverifiedMarker;
-
-    table1.push(
+    stepTable.push(
         rowTemplate
             .replace('{{FROM LEVEL}}', entry.from)
             .replace('{{TO LEVEL}}', entry.to)
-            .replace('{{MARKER}}', marker)
+            .replace('{{MARKER}}', entry.verified?'':unverifiedMarker)
             .replace('{{COST}}', regentCost),
     );
-
-    tsv1.push([
+    maxTable.push(
+        rowTemplate
+            .replace('{{FROM LEVEL}}', entry.from)
+            .replace('{{TO LEVEL}}', '2000')
+            .replace('{{MARKER}}', entry.verified?'':unverifiedMarker)
+            .replace('{{COST}}', maxRegentCost),
+    );
+    tsv.push([
       entry.from,
       entry.to,
       regentCost,
       entry.verified?'TRUE':'FALSE',
     ].join('\t'));
-
-    table2.push(
-        rowTemplate
-            .replace('{{FROM LEVEL}}', entry.from)
-            .replace('{{TO LEVEL}}', '2000')
-            .replace('{{MARKER}}', marker)
-            .replace('{{COST}}', maxRegentCost),
-    );
-
     cumulativeRegentCost += regentCost;
-  }); // for each
+  };
 
-  writeFileSync(
-      resolve(TemplatePath, './--upgrade-one-level.md'),
-      table1.join('\n'),
-  );
+  const upgradeData = database['upgrade-data'];
+  for (let i=0; i<500; i++) makeRow(upgradeData[i], stepTable1, maxTable1);
+  for (let i=500; i<1000; i++) makeRow(upgradeData[i], stepTable2, maxTable2);
+  for (let i=1000; i<1500; i++) makeRow(upgradeData[i], stepTable3, maxTable3);
+  for (let i=1500; i<2000; i++) makeRow(upgradeData[i], stepTable4, maxTable4);
 
-  writeFileSync(
-      resolve(TemplatePath, './--upgrade-max.md'),
-      table2.join('\n'),
-  );
-
-  writeFileSync(
-      resolve(ExportPath, './light-magic/light-magic-upgrade.tsv'),
-      tsv1.join('\n'),
-  );
+  writeFileSync(resolve(TemplatePath, './--upgrade-1.md'), stepTable1.join('\n'));
+  writeFileSync(resolve(TemplatePath, './--upgrade-2.md'), stepTable2.join('\n'));
+  writeFileSync(resolve(TemplatePath, './--upgrade-3.md'), stepTable3.join('\n'));
+  writeFileSync(resolve(TemplatePath, './--upgrade-4.md'), stepTable4.join('\n'));
+  writeFileSync(resolve(TemplatePath, './--upgrade-max-1.md'), maxTable1.join('\n'));
+  writeFileSync(resolve(TemplatePath, './--upgrade-max-2.md'), maxTable2.join('\n'));
+  writeFileSync(resolve(TemplatePath, './--upgrade-max-3.md'), maxTable3.join('\n'));
+  writeFileSync(resolve(TemplatePath, './--upgrade-max-4.md'), maxTable4.join('\n'));
+  writeFileSync(ExportFilePath, tsv.join('\n'));
 }
 
 buildUpgradeTable();
