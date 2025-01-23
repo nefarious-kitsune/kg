e88f09c694da322efbecd0532002d1086db61a05
kitsune <aow.anti.hero@gmail.com>
2025-01-23 09:25:26 +0800

    split html output to 4 tables

--- source/city/witchs-lab/__build/build-light-power-table.js
+++ source/city/witchs-lab/__build/build-light-power-table.js
@@ -5,19 +5,23 @@ import {readFileSync, writeFileSync} from 'fs';
 const ModulePath = dirname(fileURLToPath(import.meta.url));
 const ProjectPath = resolve(ModulePath, '../../../../');
 const TemplatePath = resolve(ModulePath, '../__templates/');
-const ExportPath = resolve(ProjectPath, './docs/city/witchs-lab/');
+const ExportPath = resolve(ProjectPath, './docs/city/witchs-lab/light-magic/');
+const ExportFilePath = resolve(ExportPath, './light-magic-power.tsv');
 
 import {database} from './build-light-data.js';
 
 /** HTML for power */ const table1 = [];
 /** HTML for power */ const table2 = [];
-/** TSV for power */ const tsv1 = [];
+/** HTML for power */ const table3 = [];
+/** HTML for power */ const table4 = [];
+/** TSV for power */ const tsv = [];
 
 /**
  * Build upgrade table
  */
 function buildUpgradeTable() {
-  tsv1.push(['level', 'light magic power', 'verified'].join('\t'));
+  // Build header row
+  tsv.push(['level', 'light magic power', 'verified'].join('\t'));
 
   const unverifiedMarker = '<img\n' +
       '  src="/assets/emojis/4x/question-mark.png"\n' +
@@ -28,31 +32,33 @@ function buildUpgradeTable() {
       {encoding: 'utf8'},
   );
 
-  database['power-data'].forEach((entry, idx) => {
-    const marker = entry.verified?'':unverifiedMarker;
-    const table = (idx < 1000)?table1:table2;
-
+  const makeRow = (entry, table) => {
     table.push(
         rowTemplate
             .replace('{{FROM LEVEL}}', entry.level)
-            .replace('{{MARKER}}', marker)
+            .replace('{{MARKER}}', entry.verified?'':unverifiedMarker)
             .replace('{{POWER}}', entry.power),
     );
-
-    tsv1.push([
+    tsv.push([
       entry.level,
       entry.power,
       entry.verified?'TRUE':'FALSE',
     ].join('\t'));
-  }); // for each
+  };
+
+
+  const powerData = database['power-data'];
+  for (let i=1; i<501; i++) makeRow(powerData[i], table1);
+  for (let i=501; i<1001; i++) makeRow(powerData[i], table2);
+  for (let i=1001; i<1501; i++) makeRow(powerData[i], table3);
+  for (let i=1501; i<2001; i++) makeRow(powerData[i], table4);
 
   writeFileSync(resolve(TemplatePath, './--power-1.md'), table1.join('\n'));
   writeFileSync(resolve(TemplatePath, './--power-2.md'), table2.join('\n'));
+  writeFileSync(resolve(TemplatePath, './--power-3.md'), table3.join('\n'));
+  writeFileSync(resolve(TemplatePath, './--power-4.md'), table4.join('\n'));
 
-  writeFileSync(
-      resolve(ExportPath, './light-magic/light-magic-power.tsv'),
-      tsv1.join('\n'),
-  );
+  writeFileSync(ExportFilePath, tsv.join('\n'));
 }
 
 buildUpgradeTable();
