e5bcac59cffbf6e15eb61746b930d8101369a018
kitsune <aow.anti.hero@gmail.com>
2025-03-11 10:12:23 +0800

    fixing problem of missing trailing '/' in url request

--- test/test.js
+++ test/test.js
@@ -8,32 +8,38 @@ const SitePath = resolve(ModulePath, '../docs/');
 
 const port = process.argv[2] || 9000;
 
+// Mapping of file extension to content-type in HTTP response head
+const contentTypeMap = {
+  // Text
+  '.html': 'text/html; charset=utf-8',
+  '.js': 'text/javascript; charset=utf-8',
+  '.json': 'application/json; charset=utf-8',
+  '.css': 'text/css; charset=utf-8',
+  // Binary
+  '.ico': 'image/x-icon',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.svg': 'image/svg+xml',
+  '.pdf': 'application/pdf',
+};
+const defaultContentType = 'text/plain;  charset=utf-8';
+
 http.createServer((req, res) => {
   console.log(`${req.method} ${req.url}`);
 
-  const pathname = `.${parseUrl(req.url).pathname}`;
+  const pathname = parseUrl(req.url).pathname;
   let ext = extname(pathname);
 
-  // Mapping of file extension to content-type in HTTP response head
-  const contentTypeMap = {
-    // Text
-    '.html': 'text/html; charset=utf-8',
-    '.js': 'text/javascript; charset=utf-8',
-    '.json': 'application/json; charset=utf-8',
-    '.css': 'text/css; charset=utf-8',
-    // Binary
-    '.ico': 'image/x-icon',
-    '.png': 'image/png',
-    '.jpg': 'image/jpeg',
-    '.svg': 'image/svg+xml',
-    '.pdf': 'application/pdf',
-  };
-  const defaultContentType = 'text/plain;  charset=utf-8';
+  let localPath = resolve(SitePath, `.${pathname}`);
 
   // URL rewriting...
-  let localPath = resolve(SitePath, pathname);
   if (existsSync(localPath)) {
     if (statSync(localPath).isDirectory()) {
+      if (pathname.slice(-1) !== '/') {
+        console.log(`Redirecting '${pathname}' to '${pathname}/`);
+        res.writeHead(301, {Location: `${pathname}/`}).end();
+        return;
+      }
       localPath += '/index.html';
       if (!existsSync(localPath)) {
         res.statusCode = 404;
