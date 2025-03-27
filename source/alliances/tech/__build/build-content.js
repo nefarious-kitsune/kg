dfa17a1dbf274cfc7dc9b6eea4f91956d0f153f1
kitsune <aow.anti.hero@gmail.com>
2025-03-27 12:31:31 +0800

    alliance tech

--- source/alliances/tech/__build/build-content.js
+++ source/alliances/tech/__build/build-content.js
@@ -15,17 +15,36 @@ const snippetTemplate = readFileSync(
 const upgradeRowTemplate = readFileSync(
     resolve(TemplatePath, './tech-upgrade-line.html'), 'utf8');
 
+const labelTemplate = readFileSync(
+    resolve(TemplatePath, './tech-label.html'), 'utf8');
+
 const hints = [];
+const labels = [];
+
+const classes = [
+  'population',
+  'gathering',
+  'gold',
+  'expansion',
+  'protection',
+  'gift',
+  'capacity',
+  'recovery',
+  'march',
+  'power',
+];
 
 /**
  * Build tiered Hero Gear content
  * @param {string[]} data
- * @param {string} id
  */
-function buildTechContent(data, id) {
+function buildTechContent(data) {
   const title = data.shift();
+  const id = title.toLowerCase().replaceAll(' ', '-');
   const effect = data.shift();
   const researchTime = data.shift();
+
+  const techClass = classes.find((c) => id.includes(c));
 
   const upgradeBody = data.map((cost, index) => upgradeRowTemplate
       .replace('{{PREV LEVEL}}', index)
@@ -39,6 +58,13 @@ function buildTechContent(data, id) {
       .replace('{{EFFECT}}', effect)
       .replace('{{RESEARCH TIME}}', researchTime)
       .replace('{{UPGRADE DATA}}', upgradeBody.join('\n'))
+      ,
+  );
+
+  labels.push(labelTemplate
+      .replace('{{ID}}', id)
+      .replace('{{TITLE}}', title)
+      .replace('{{CLASS}}', techClass?`${techClass}-tech`:'')
       ,
   );
 }
@@ -59,22 +85,50 @@ function extract(rowIdx, colIdx, levels) {
 }
 
 let rowStart = 0;
+
 let levels = 5;
-buildTechContent(extract(rowStart, 0, levels), 'population-1');
-buildTechContent(extract(rowStart, 1, levels), 'gathering-1');
-buildTechContent(extract(rowStart, 2, levels), 'gold-1');
-buildTechContent(extract(rowStart, 3, levels), 'territory-1');
+buildTechContent(extract(rowStart, 0, levels));
+buildTechContent(extract(rowStart, 1, levels));
+buildTechContent(extract(rowStart, 2, levels));
+buildTechContent(extract(rowStart, 3, levels));
+writeFileSync(resolve(OutputPath, `./labels-1.html`), labels.join('\n'));
+labels.length = 0;
+
 rowStart += (levels + 3);
+
 
 levels = 1;
-buildTechContent(extract(8, 3, 1), 'protection');
+buildTechContent(extract(8, 3, 1));
+writeFileSync(resolve(OutputPath, `./labels-p.html`), hints.join('\n'));
+labels.length = 0;
+
 rowStart += (levels + 3);
 
+// Tier 2 Tech
 levels = 5;
-buildTechContent(extract(rowStart, 0, levels), 'gift-1');
-buildTechContent(extract(rowStart, 1, levels), 'capacity-1');
-buildTechContent(extract(rowStart, 2, levels), 'recovery-1');
-buildTechContent(extract(rowStart, 3, levels), 'territory-2');
+buildTechContent(extract(rowStart, 0, levels));
+buildTechContent(extract(rowStart, 1, levels));
+buildTechContent(extract(rowStart, 2, levels));
+buildTechContent(extract(rowStart, 3, levels));
+writeFileSync(resolve(OutputPath, `./labels-2.html`), labels.join('\n'));
+labels.length = 0;
+
 rowStart += (levels + 3);
+
+// Tier 3–10 Tech
+for (let techTier = 3; techTier <= 10; techTier++) {
+  buildTechContent(extract(rowStart, 0, levels));
+  buildTechContent(extract(rowStart, 1, levels));
+  buildTechContent(extract(rowStart, 2, levels));
+  buildTechContent(extract(rowStart, 3, levels));
+
+  writeFileSync(
+      resolve(OutputPath, `./labels-${techTier}.html`),
+      labels.join('\n'),
+  );
+  labels.length = 0;
+
+  rowStart += (levels + 3);
+}
 
 writeFileSync(resolve(OutputPath, `./hints.html`), hints.join('\n'));
