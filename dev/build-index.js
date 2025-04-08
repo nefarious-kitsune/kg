839061a913a786393c65341f0617d279385a8fda
kitsune <aow.anti.hero@gmail.com>
2025-04-08 12:07:35 +0800

    WIP front-matter processing

--- dev/build-index.js
+++ dev/build-index.js
@@ -1,24 +1,46 @@
 import {extname} from 'path';
 import {readFileSync} from 'fs';
 import {findContentFiles} from './utils/file-utils.js';
+import YAML from 'yaml';
 
-import {processYaml} from './yaml/yaml-processor.js';
 import * as logger from './logger.js';
 import * as consts from './consts.js';
-
 
 const loadFile = (path) => readFileSync(path, 'utf8');
 // const saveFile = (path, content) => writeFileSync(path, content);
 
-const filter = (fn) => extname(fn) === '.yaml';
+/** @typedef {import('../utils/file-utils.js').FileResult} FileResult */
+
+/**
+ * @param {string} srcContent - source content
+ * @param {FileResult} fResult - file search result
+ * @return {object} - processed front matter
+ */
+function getFrontMatter(srcContent, fResult) {
+  srcContent = srcContent.replaceAll('\r', ''); // Remove all \r characters
+  const separator = '---\n';
+  if (!srcContent.startsWith(separator)) return null;
+  const fmStart = separator.length;
+  const fmEnd = srcContent.indexOf(separator, fmStart);
+  if (fmEnd < 0) return null;
+
+  const fmContent = srcContent.slice(fmStart, fmEnd);
+  const frontMatter = YAML.parse(fmContent);
+  frontMatter.shortTitle = frontMatter.shortTitle || frontMatter.title;
+  const sourceUrl = '/' + fResult.relUrl;
+  frontMatter.sourceUrl = sourceUrl;
+  frontMatter.permaLink = frontMatter.permaLink || sourceUrl;
+  return frontMatter;
+}
 
 /** Build page indexes */
 function buildIndex() {
-  const yamlFiles = findContentFiles(consts.sourceDir, filter);
-  yamlFiles.forEach((file) => {
-    const content = loadFile(file.fullPath);
-    const meta = processYaml(content, file);
-    logger.log(JSON.stringify(meta));
+  const filter = (fn) => extname(fn) === '.md';
+  const mdFiles = findContentFiles(consts.sourceDir, filter);
+  mdFiles.forEach((file) => {
+    const srcContent = loadFile(file.fullPath);
+    const frontMatter = getFrontMatter(srcContent, file);
+    if (frontMatter) logger.log(JSON.stringify(frontMatter));
   });
 }
 
